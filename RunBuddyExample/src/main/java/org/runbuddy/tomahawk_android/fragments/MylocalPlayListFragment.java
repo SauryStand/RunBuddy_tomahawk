6f82c2da268670c4e4cac5d0a6cbdbd717ed95bb
wohuodong <janfongz@126.com>
2016-07-26 21:49:49 +0800

    I want to add a LocalPlayList

---
+++ RunBuddyExample/src/main/java/org/runbuddy/tomahawk_android/fragments/MylocalPlayListFragment.java
@@ -0,0 +1,10 @@
+package org.runbuddy.tomahawk_android.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by Jonney Chou on 2016/7/26.
+ */
+public class MylocalPlayListFragment extends Fragment {
+
+}
