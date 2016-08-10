bd66ece17093d7cf9f1c67713284bc6e1d6a5f48
voyager <voyager2511@outlook.com>
2016-08-10 17:52:38 +0800

    still have some problem

---
+++ RunBuddyExample/src/main/java/org/runbuddy/tomahawk_android/demo_zhihu/event/TabSelectedEvent.java
@@ -0,0 +1,12 @@
+package org.runbuddy.tomahawk_android.demo_zhihu.event;
+
+/**
+ * Created by YoKeyword on 16/6/5.
+ */
+public class TabSelectedEvent {
+    public int position;
+
+    public TabSelectedEvent(int position) {
+        this.position = position;
+    }
+}
