2f427c23fd1b22c0195d83abad672541d85babb8
wohuodong <janfongz@126.com>
2016-08-16 21:58:36 +0800

    add jni and build UI layout

--- RunBuddyExample/src/main/java/org/runbuddy/Device/GravitySensor/GravitySensorActivity.java
+++ RunBuddyExample/src/main/java/org/runbuddy/Device/GravitySensor/GravitySensorActivity.java
@@ -2,21 +2,44 @@ package org.runbuddy.Device.GravitySensor;
 
 import android.app.Activity;
 import android.hardware.SensorEvent;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-
+import android.widget.TextView;
 import org.runbuddy.Device.CounterSensor.SensorHub;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Created by Johnny Chow on 2016/8/15.
  */
 public class GravitySensorActivity extends Activity implements SensorHub.DataClient {
 
+    private TextView mtextView;
+    GLSurfaceView glSurfaceView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //// TODO: 2016/8/15
+        glSurfaceView = new GLSurfaceView(getApplication());
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+                //AccelerometerGraphJNI.surfaceCreated();
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl) {
+
+            }
+        });
 
     }
 
