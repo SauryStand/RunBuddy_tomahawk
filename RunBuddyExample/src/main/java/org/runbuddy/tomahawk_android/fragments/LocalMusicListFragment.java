9412b1b065225337a97b2e1aaabe0a96730b0bf4
wohuodong <janfongz@126.com>
2016-08-19 21:12:10 +0800

    clean some codes

--- RunBuddyExample/src/main/java/org/runbuddy/tomahawk_android/fragments/LocalMusicListFragment.java
+++ RunBuddyExample/src/main/java/org/runbuddy/tomahawk_android/fragments/LocalMusicListFragment.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
 import org.runbuddy.Device.CounterSensor.SensorActivity;
 import org.runbuddy.Device.CounterSensor.SensorListAdapter;
+import org.runbuddy.Intricate_Charts.ListViewMultiChartActivity;
 import org.tomahawk.tomahawk_android.R;
 
 /**
@@ -19,7 +21,8 @@ import org.tomahawk.tomahawk_android.R;
  */
 public class LocalMusicListFragment extends Fragment implements View.OnClickListener {
 
-    private Button clickme;
+    private ImageButton step_count_btn;
+    private Button Charts;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -27,37 +30,45 @@ public class LocalMusicListFragment extends Fragment implements View.OnClickList
         getActivity().setTitle(R.string.drawer_title_localList);
         View view = inflater.inflate(R.layout.zhihu_fragment_first_detail, container, false);
         initialView(view);
-        //clickme = (Button) view.findViewById(R.id.click_me);
-        //clickme.setOnClickListener(this);
-
-
-        //to do list
-
-
-
         return view;
     }
 
     private void initialView(View view) {
-        clickme = (Button) view.findViewById(R.id.click_me);
-        clickme.setOnClickListener(this);
-
+        step_count_btn = (ImageButton) view.findViewById(R.id.step_counting_btn);
+        step_count_btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    view.setBackgroundResource(R.drawable.running_48px_gray);
+                    SensorListAdapter.sensorType(16);//16是step counter
+                    Bundle data = new Bundle();
+                    mIntent = new Intent(getContext(), SensorActivity.class);
+                    data.putLong("sensorID", 4864);
+                    mIntent.putExtras(data);
+                    getActivity().startActivity(mIntent);
+                    //Toast.makeText(getContext(), "need to add sensor counter", Toast.LENGTH_SHORT).show();
+                }else if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    view.setBackgroundResource(R.drawable.running_48px);
+                }
+                return false;
+            }
+        });
+        Charts = (Button)view.findViewById(R.id.charts_info);
+        Charts.setOnClickListener(this);
     }
 
 
-    private Intent mIntent;
+    private Intent mIntent,mIntent2;
 
     @Override
     public void onClick(View v) {
 
-        if (v.getId() == R.id.click_me) {
-            SensorListAdapter.sensorType(16);//16是step counter
-            Bundle data = new Bundle();
-            mIntent = new Intent(getContext(), SensorActivity.class);
-            data.putLong("sensorID", 4864);
-            mIntent.putExtras(data);
-            getActivity().startActivity(mIntent);
-            Toast.makeText(getContext(), "need to add sensor counter", Toast.LENGTH_SHORT).show();
+        if (v.getId() == R.id.step_counting_btn) {
+
+        }
+        if(v.getId() == R.id.charts_info){
+            mIntent2 = new Intent(getActivity(), ListViewMultiChartActivity.class);
+            startActivity(mIntent2);
         }
 
 
