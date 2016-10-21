fd9b238fc532763d9be45eb2d06dc49fc60aac36
wohuodong <janfongz@126.com>
2016-10-22 00:05:10 +0800

    attempt to add gank.io api data in star_page

--- RunBuddyExample/src/main/java/org/runbuddy/tomahawk/ui/fragments/star_page/StarPageFragment.java
+++ RunBuddyExample/src/main/java/org/runbuddy/tomahawk/ui/fragments/star_page/StarPageFragment.java
@@ -1,7 +1,10 @@
 package org.runbuddy.tomahawk.ui.fragments.star_page;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -13,7 +16,11 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.runbuddy.tomahawk.R;
 import org.runbuddy.tomahawk.ui.fragments.star_page.widgets.DetailActivity;
 import org.runbuddy.tomahawk.ui.fragments.star_page.widgets.RecyclerViewAdapter;
@@ -29,13 +36,13 @@ public class StarPageFragment extends Fragment implements NavigationView.OnNavig
 
     private static final int PAGE_SIZE = 15;
     public static final String AVATAR_URL = "http://lorempixel.com/200/200/people/1/";
-
+    protected Thread mThread;
     private static List<ViewModel> items = new ArrayList<>();
 
     static {
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 0; i <= 15; i++) {
             //items.add(new ViewModel("User:" + i, "http://www.voyager2511.top/temp_file/images/" + i + ".jpg"));
-            items.add(new ViewModel("test:" + i , "http://gank.io/api/data/%E7%A6%8F%E5%88%A9/" + PAGE_SIZE + "/" + i));
+            items.add(new ViewModel("test:" + i, "http://gank.io/api/data/%E7%A6%8F%E5%88%A9/" + PAGE_SIZE + "/" + i));
             //这里需要换个图片才行
         }
     }
@@ -61,8 +68,65 @@ public class StarPageFragment extends Fragment implements NavigationView.OnNavig
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             setRecyclerAdapter(recyclerView);
         }
+        mThread = new Thread(runnable);
+        mThread.start();
         return view;
     }
+
+    private void catchGankImage() {
+        try {
+
+            JSONArray jsonArray = new JSONArray("http://gank.io/api/data/%E7%A6%8F%E5%88%A9/15/2");
+            int temp = 0;
+            for (int i = 0; i < jsonArray.length(); i++) {
+                temp++;
+            }
+            //JSONObject jsonObject = new JSONObject("http://gank.io/api/data/%E7%A6%8F%E5%88%A9/15/2");
+            //String temp = jsonObject.get("who").toString();
+            Toast.makeText(getActivity(), "----->>" + temp + "", Toast.LENGTH_SHORT).show();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void testingImage() {
+
+    }
+    /**************************************/
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            URLServer server = new URLServer(mHandler);
+            server.getImage();
+        }
+    };
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage (Message msg) {
+            switch(msg.what) {
+                case 0:
+                    execute(msg);
+                    break;
+                case 1:
+                    break;
+            }
+        }
+
+        private void execute(Message msg) {
+            try {
+                JSONObject resultJson = new JSONObject(msg.obj.toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+    /***************************************/
 
 
     public void onEnterAnimationComplete() {
