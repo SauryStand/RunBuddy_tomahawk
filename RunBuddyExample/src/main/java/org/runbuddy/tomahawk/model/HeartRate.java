11787e76df9023ff3f898439963b535df11abd54
SauryStand <voyager2511@outlook.com>
2016-11-17 23:25:27 +0800

    add MVP-model file and learn how to use cmake in project!

--- RunBuddyExample/src/main/java/org/runbuddy/tomahawk/model/HeartRate.java
+++ RunBuddyExample/src/main/java/org/runbuddy/tomahawk/model/HeartRate.java
@@ -5,4 +5,13 @@ package org.runbuddy.tomahawk.model;
  */
 
 public class HeartRate {
+    private String heart_rate;
+
+    public String getHeart_rate() {
+        return heart_rate;
+    }
+
+    public void setHeart_rate(String heart_rate) {
+        this.heart_rate = heart_rate;
+    }
 }
